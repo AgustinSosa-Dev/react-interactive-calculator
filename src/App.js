582af663c22401b518e0f7cd6a2ad6f5f194ed50
Agustín Sosa-Dev <agustinsosa.dev@gmail.com>
2022-12-23 14:44:07 -0300

    Release 1.0.0

--- src/App.js
+++ src/App.js
@@ -1,23 +1,65 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import freeCodeCampLogo from "./assets/images/freecodecamp-logo.png";
+import Button from "./components/Button";
+import Display from "./components/Display";
+import ClearButton from "./components/ClearButton";
+import { useState } from "react";
+import { evaluate } from "mathjs";
 
 function App() {
+  const [input, setInput] = useState("");
+
+  const addInput = (val) => {
+    setInput(input + val);
+  };
+
+  const calculateResult = () => {
+    if (input) {
+      setInput(evaluate(input));
+    } else {
+      alert("Please enter values ​​to perform the calculations.");
+    }
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="freecodecamp-logo-container">
+        <img
+          src={freeCodeCampLogo}
+          className="freecodecamp-logo"
+          alt="freeCodeCamp logo"
+        />
+      </div>
+      <div className="calculator-container">
+        <Display input={input} />
+        <div className="row">
+          <Button handleClick={addInput}>1</Button>
+          <Button handleClick={addInput}>2</Button>
+          <Button handleClick={addInput}>3</Button>
+          <Button handleClick={addInput}>+</Button>
+        </div>
+        <div className="row">
+          <Button handleClick={addInput}>4</Button>
+          <Button handleClick={addInput}>5</Button>
+          <Button handleClick={addInput}>6</Button>
+          <Button handleClick={addInput}>-</Button>
+        </div>
+        <div className="row">
+          <Button handleClick={addInput}>7</Button>
+          <Button handleClick={addInput}>8</Button>
+          <Button handleClick={addInput}>9</Button>
+          <Button handleClick={addInput}>*</Button>
+        </div>
+        <div className="row">
+          <Button handleClick={calculateResult}>=</Button>
+          <Button handleClick={addInput}>0</Button>
+          <Button handleClick={addInput}>.</Button>
+          <Button handleClick={addInput}>/</Button>
+        </div>
+        <div className="row">
+          <ClearButton handleClear={() => setInput("")}>Clear</ClearButton>
+        </div>
+      </div>
     </div>
   );
 }
---
+++ src/components/Button.js
@@ -0,0 +1,21 @@
+import React from "react";
+import "../style/Button.css";
+
+function Button(props) {
+  const isOperator = (valor) => {
+    return isNaN(valor) && valor !== "." && valor !== "=";
+  };
+
+  return (
+    <div
+      className={`button-container ${
+        isOperator(props.children) ? "operator" : ""
+      }`.trimEnd()}
+      onClick={() => props.handleClick(props.children)}
+    >
+      {props.children}
+    </div>
+  );
+}
+
+export default Button;
